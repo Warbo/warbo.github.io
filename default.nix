415d5ba1666bdef9754ba9c2028437a51729a333
Chris Warburton <chriswarbo@gmail.com>
2021-04-18 20:24:48 +0100

    Added more pages

---
+++ default.nix
@@ -0,0 +1,30 @@
+with builtins;
+with import <nixpkgs> {};
+with lib;
+with rec {
+  bodies = mapAttrs'
+    (name: value: { inherit value; name = removeSuffix "-body.html" name; })
+    (genAttrs
+      (filter (hasSuffix "-body.html") (attrNames (readDir ./.)))
+      (f: ./. + "/${f}"));
+
+  compiled = mapAttrs
+    (name: bodyPath:  ''
+      <html>
+        ${readFile ./head.html}
+        <body>
+          ${readFile ./header.html}
+          ${readFile bodyPath}
+        </body>
+      </html>
+    '')
+    bodies;
+
+  pages = mapAttrs'
+    (page: content: rec {
+      name  = page + ".html";
+      value = writeText name content;
+    })
+    compiled;
+};
+attrsToDirs' "wedding-site" (pages // { resources = ./resources; })
