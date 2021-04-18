03ba4622ccda471541bac355cf802ebc576ff099
Chris Warburton <chriswarbo@gmail.com>
2021-04-18 21:39:15 +0100

    Add RSVP redirect

--- default.nix
+++ default.nix
@@ -8,6 +8,21 @@ with rec {
       (filter (hasSuffix "-body.html") (attrNames (readDir ./.)))
       (f: ./. + "/${f}"));
 
+  rsvp =
+    with {
+      url = "http://example.com";
+    };
+    writeText "rsvp.html" ''
+      <html>
+        <head>
+          <meta http-equiv="refresh" content="0; url=${url}">
+        </head>
+        <body onload="window.location = '${url}'">
+          <a href="${url}">CLICK HERE FOR RSVP FORM</a>
+        </body>
+      </html>
+    '';
+
   compiled = mapAttrs
     (name: bodyPath:  ''
       <html>
