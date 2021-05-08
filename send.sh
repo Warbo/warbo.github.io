4a9eccd5a767417312ac8b643e960e2d369e2264
Chris Warburton <chriswarbo@gmail.com>
2021-05-08 16:12:17 +0100

    Ignore addresses

---
+++ send.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+shopt -s nullglob
+
+cat addresses.tsv | while read -r DETAILS
+do
+       NAME=$(echo "$DETAILS" | cut -f1)
+    ADDRESS=$(echo "$DETAILS" | cut -f2)
+    echo "Sending to $NAME at $ADDRESS" 1>&2
+    sed -e "s/TONAME/$NAME/g" -e "s/TOADDRESS/$ADDRESS/g" < invite.raw |
+        msmtp -a gmail --read-envelope-from --read-recipients
+done
